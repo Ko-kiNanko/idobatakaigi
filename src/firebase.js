0f2619963d3636f3f08ad9d71b2d46d1a39e5b87
ko-kiNanko <mcoke0131@gmail.com>
2021-08-04 19:33:01 +0900

    remove hard coaded env vars

--- src/firebase.js
+++ src/firebase.js
@@ -1,12 +1,23 @@
 import firebase from 'firebase';
 
+const {
+  REACT_APP_FIREBASE_API_KEY,
+  REACT_APP_FIREBASE_AUTH_DOMAIN,
+  REACT_APP_FIREBASE_DATABASE_URL,
+  REACT_APP_FIREBASE_PROJECT_ID,
+  REACT_APP_FIREBASE_STORAGE_BUCKET,
+  REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  REACT_APP_FIREBASE_APP_ID,
+} = process.env;
+
 const firebaseConfig = {
-  apiKey: "AIzaSyCq3gU3EhC25RF_8RpmPvz5DAUtGlUOT6Q",
-  authDomain: "idobatakaigi-with-ham-80e4b.firebaseapp.com",
-  projectId: "idobatakaigi-with-ham-80e4b",
-  storageBucket: "idobatakaigi-with-ham-80e4b.appspot.com",
-  messagingSenderId: "1007112162669",
-  appId: "1:1007112162669:web:58f8098ba90d475f42de64"
+  apiKey: REACT_APP_FIREBASE_API_KEY,
+  authDomain: REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: REACT_APP_FIREBASE_APP_ID,
 };
 
 firebase.initializeApp(firebaseConfig);
